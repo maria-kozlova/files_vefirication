fa4b4207729bc2e6e149e7f6df53870cf0b71f6e
Maria <marimuur08@gmail.com>
2021-04-06 17:37:32 -0700

    added java file and test case

---
+++ src/test/java/tests/CommitTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class CommitTest {
+
+    @Test
+    public void testForCommits() {
+        System.out.println("First commit");
+    }
+}
