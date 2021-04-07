3421c90a49b028979b0bd1e016308c0bde098778
Maria <marimuur08@gmail.com>
2021-04-06 17:40:22 -0700

    first commit

--- src/test/java/tests/CommitTest.java
+++ src/test/java/tests/CommitTest.java
@@ -6,7 +6,7 @@ public class CommitTest {
 
     @Test
     public void testForCommits() {
-        System.out.println("First commit");
+        System.out.println("My First commit");
         System.out.println("Second commit");
         System.out.println("Third commit");
     }
